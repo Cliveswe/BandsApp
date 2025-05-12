b63e7cf7ee75a07aa6620fb325a5dd3cdba77e68
Nidora <ludwigjohansson@gmail.com>
2025-05-12 16:15:44 +0200

    added models and service

---
+++ BandsApp.Web/Models/Band.cs
@@ -0,0 +1,15 @@
+﻿namespace BandsApp.Web.Models;
+
+public class Band
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string  Description { get; set; }
+
+    public Band(int id, string name, string description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+}
---
+++ BandsApp.Web/Services/BandService.cs
@@ -0,0 +1,24 @@
+﻿using BandsApp.Web.Models;
+
+namespace BandsApp.Web.Services;
+
+public class BandService
+{
+    private List<Band> bands = [
+        new Band(1, "Misery Business", "rock music"),
+        new Band(2, "E-Type", "swedish"),
+        new Band(3, "Slipknot", "metal"),
+    ];
+
+    public Band? GetById(int id)
+        => bands
+        .SingleOrDefault(b => b.Id == id);
+
+
+    public Band[] GetAll()
+    {
+        return bands
+            .OrderBy(x => x.Name)
+            .ToArray();
+    }
+}
