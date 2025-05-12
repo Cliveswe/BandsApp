259c757f3fbe68c88e0ed5de0bf7a553416a872d
1256ID <112385041+1256ID@users.noreply.github.com>
2025-05-12 16:54:47 +0200

    Adding Controllers/View

---
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BandsApp.Web.Services;
+using BandsApp.Web.Models;
+
+namespace BandsApp.Web.Controllers
+{
+    public class BandsController : Controller
+    {
+        BandService bandService = new();
+        [Route("")]
+        public IActionResult Index()
+        {
+            Band[] model = bandService.GetAll();
+            return View(model);
+        }
+
+        
+
+
+
+
+
+    }
+}
