3edbc68e08f6f6e61dc78cde9ae611a6693b430f
Nidora <ludwigjohansson@gmail.com>
2025-05-13 10:47:13 +0200

    albumcover progress

--- BandsApp.Web/Controllers/BandsController.cs
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -3,26 +3,21 @@ using BandsApp.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BandsApp.Web.Controllers
-    {
+{
     public class BandsController : Controller
-        {
+    {
         BandService bandService = new();
         [Route("")]
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
             Band[] model = bandService.GetAll();
             return View(model);
-            }
-
-
+        }
         [Route("/details/{id}")]
-        public IActionResult Details(int id) {
+        public IActionResult Details(int id)
+        {
             var res = bandService.GetById(id);
             return View(res);
-            }
-
-
-
-
-
         }
     }
+}
--- BandsApp.Web/Models/Band.cs
+++ BandsApp.Web/Models/Band.cs
@@ -5,11 +5,23 @@ public class Band
     public int Id { get; set; }
     public string Name { get; set; }
     public string  Description { get; set; }
-
     public Band(int id, string name, string description)
     {
         Id = id;
         Name = name;
         Description = description;
+    }
+
+    public string GetAlbumCover()
+    {
+        return "~/Images/" + Name.ToLower()
+            .Replace(" ", "")
+            .Replace("'", "")
+            .Replace("-", "")
+            .Replace(".", "")
+            .Replace("&", "")
+            .Replace("!", "")
+            .Replace(",", "")
+            + ".jpg";
     }
 }
