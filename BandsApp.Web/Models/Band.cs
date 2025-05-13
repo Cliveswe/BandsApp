2f6b29aed50f52a85dad289351ff9370413e1fe1
Clive <Cliveswe@users.noreply.github.com>
2025-05-13 13:12:24 +0200

    Added property for video and updated the constructor.

--- BandsApp.Web/Models/Band.cs
+++ BandsApp.Web/Models/Band.cs
@@ -4,18 +4,31 @@ namespace BandsApp.Web.Models;
 
 public class Band
     {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description {get; set;}
-    public string[] Albums { get; set; }
+    public int Id {
+        get; set;
+        }
+    public string Name {
+        get; set;
+        }
+    public string Description {
+        get; set;
+        }
+    public string[] Albums {
+        get; set;
+        }
 
-    public Band(int id, string name, string description, string[] albums)
-    {
+    public string Video {
+        get;
+        set;
+        }
+
+    public Band(int id, string name, string description, string[] albums, string video) {
         Id = id;
         Name = name;
         Description = description;
         Albums = albums;
-    }
+        Video = video;
+        }
 
     public string GetAlbumCover() {
         StringBuilder path = new();
